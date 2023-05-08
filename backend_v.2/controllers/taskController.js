75ec22668603e36eb0ff79d254585f8231ed919b
Baten <yegor.sibryayev@mail.ru>
2023-05-08 14:10:35 +0400

    что-то

--- backend_v.2/controllers/taskController.js
+++ backend_v.2/controllers/taskController.js
@@ -8,7 +8,7 @@ const app = express();
 
 class tasksPageController{
 
-    async task_page(req, res) {
+    async task_get(req, res) {
         const id = req.params.id;
         pool.query("SELECT * FROM `own tasks` WHERE id=?", [id], function (err, data) {
             if (err) return console.log(err);
@@ -17,6 +17,17 @@ class tasksPageController{
                 data: data
             });
         });
+    }
+    async task_edit(req, res) {
+        const {id} = req.query;
+        let data = req.body;
+        res.json(data);
+    }
+    async task_delete(req, res) {
+        const {id} = req.query;
+        res.json({
+            "message": `удалить ${id}`
+        })
     }
 }
 
--- backend_v.2/rotes/taskRouter.js
+++ backend_v.2/rotes/taskRouter.js
@@ -2,6 +2,8 @@ const Router = require('express');
 const router = new Router();
 const taskController = require('../controllers/taskController');
 
-router.get('/:id', taskController.task_page);
+router.get('/:id', taskController.task_get);
+router.post('/', taskController.task_edit);
+router.delete('/', taskController.task_delete);
 
 module.exports = router;
