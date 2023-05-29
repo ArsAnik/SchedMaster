6de0602e1e5e435221b8877a1df15cc12bab0af9
FedorKam <116003118+ArsAnik@users.noreply.github.com>
2023-05-29 22:35:18 +0400

    Header

--- frontend/src/components/HeaderComponent/HeaderComponent.js
+++ frontend/src/components/HeaderComponent/HeaderComponent.js
@@ -1,41 +1,60 @@
-import React from "react";
+import React, {useState} from "react";
 import './headerTSC-style.css'
 import './HeaderComponent.css'
+import logo from '../../pictures/logo.svg';
 
 export default function HeaderComponent(props){
-    return(
-        <header className="header">
-            <div className="header-text">
-                <div className="header-text-task active">
+    const [open, setOpen] = useState<boolean>(false);
+    const close = () => setOpen(false);
+    if(window.screen.availWidth > 1162){
+        return(
+            <header className="header">
+                <div className="header-text">
+                    <div className="header-text-task active">
 
-                    <a className="header-text-task-button" href="@">Задачи</a>
+                        <a className="header-text-task-button" href="/tasksPage">Задачи</a>
 
-                </div>
+                    </div>
 
-                <div className="header-text-schedule">
-                    <div className="header-text-schedule-container">
+                    <div className="header-text-schedule">
+                        <div className="header-text-schedule-container">
 
-                        <a className="header-text-schedule-button" href="@">Расписание</a>
+                            <a className="header-text-schedule-button" href="/schedule">Расписание</a>
+                        </div>
+
+                    </div>
+
+                    <div className="header-text-calendar">
+
+                        <a className="header-text-calendar-button" href="/calendar">Календарь</a>
+
                     </div>
 
                 </div>
 
-                <div className="header-text-calendar">
+                <a className="open-personalWindow without-avatar" href="@">
+                    <span className="personLetter">Я</span>
 
-                    <a className="header-text-calendar-button" href="@">Календарь</a>
+                </a>
+
+                <div className="header-line">
 
                 </div>
+            </header>
+        )
+    }
+    else
+    {
+        return(
+            <header className="header_mob">
+                <div className="menu-block">
 
-            </div>
-
-            <a className="open-personalWindow without-avatar" href="@">
-                <span className="personLetter">Я</span>
-
-            </a>
-
-            <div className="header-line">
-
-            </div>
-        </header>
-    )
+                </div>
+                <div className="header_top">
+                    <img src={logo}/>
+                    <span className="header_top_text">Sched-master</span>
+                </div>
+            </header>
+        )
+    }
 }
